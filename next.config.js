169a915beb13bdbf221644ad57fe08fd8cbafdb5
muchomo <tastelikecrypto@gmail.com>
2025-01-08 17:35:52 -0700

    added verbose logs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  webpack: (config, { isServer }) => {
+    // Log the Webpack configuration for debugging
+    console.log('Webpack Configuration:', JSON.stringify(config, null, 2));
+    
+    return config;
+  },
+};
+
+module.exports = nextConfig;
